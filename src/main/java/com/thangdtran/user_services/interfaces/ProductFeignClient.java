51f8dd095b9c4840965b59d5de6ba7fafde2243d
Thang Tran <thangdtran@kms-technology.com>
2020-04-08 17:08:14 +0700

    add endpoint openfeign

--- src/main/java/com/thangdtran/user_services/interfaces/ProductFeignClient.java
+++ src/main/java/com/thangdtran/user_services/interfaces/ProductFeignClient.java
@@ -3,9 +3,14 @@ package com.thangdtran.user_services.interfaces;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name="product-services", url = "http://localhost:8080")
 public interface ProductFeignClient {
     @GetMapping(path = "/product/{product_id}")
     String getProductInfo(@PathVariable("product_id") String productId);
+
+    @PostMapping(path="/product/{product_id}/name/{product_name}")
+    boolean updateProductName(@PathVariable("product_id") String productId,
+                              @PathVariable("product_name") String product_name);
 }
